b73e5e70fcae7660c7be48952ae03c0ee6b71cb8
jacobkrause <jacobkrause90@gmail.com>
2018-12-12 11:40:42 -0700

    Object is not NULL

---
+++ game.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jacobkrause on 12/12/2018.
+//
+
+#include "game.h"
--- main.cpp
+++ main.cpp
@@ -1,10 +1,15 @@
 #define CATCH_CONFIG_MAIN
 
 #include "catch.hpp"
+#include "game.h"
 
-TEST_CASE("TestName","[t]")
+TEST_CASE("Does the Game exist","[score]")
 {
     //Arrange
+    Game *g = new Game;
+    bool exist = true;
     //Act
+
     //Assert
+    REQUIRE(g != NULL);
 }
