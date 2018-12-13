e853efa226879db200feeca3c0faa5e1da544828
jacobkrause <jacobkrause90@gmail.com>
2018-12-13 09:05:06 -0700

    isSpare

--- game.cpp
+++ game.cpp
@@ -6,10 +6,34 @@
 
 void Game::roll(int frame)
 {
-
+    //if(currentRoll == 0)
+    rolls[currentRoll] = frame;
+    currentRoll++;
+    //score += frame;
 }
 
 int Game::scoreGame()
 {
-    return 0;
+    int score = 0;
+    int rollNum = 0;
+    // 10 scores per game
+    for(int frame = 0; frame < 10; frame++)
+    {
+        if(isSpare(rollNum))
+        {
+            score += 10 + rolls[rollNum + 2];
+        }
+        else
+        {
+            score += rolls[rollNum] + rolls[rollNum + 1];
+        }
+        // Scoring per game
+        rollNum += 2;
+    }
+    return score;
+}
+
+bool Game::isSpare(int frameIndex)
+{
+    return rolls[frameIndex] + rolls[frameIndex + 1] == 10;
 }
--- main.cpp
+++ main.cpp
@@ -15,14 +15,45 @@ TEST_CASE("Does the Game exist","[score]")
 
 TEST_CASE("Gutter Game Return 0","[score]")
 {
-    //Arrange
+//Arrange
+Game g;
+int pins = 0;
+//Act
+for(int frame = 0; frame < 20; frame++)
+{
+g.roll(pins);
+}
+//Assert
+REQUIRE(0 == g.scoreGame());
+}
+
+TEST_CASE("Single Pin Game","[score]")
+{
+//Arrange
     Game g;
-    int pins = 0;
-    //Act
+    int pins = 1;
+//Act
     for(int frame = 0; frame < 20; frame++)
     {
         g.roll(pins);
     }
-    //Assert
-    REQUIRE(0 == g.scoreGame());
+//Assert
+    REQUIRE(20 == g.scoreGame());
+}
+
+TEST_CASE("One Spare Game Returns Correct Value","[score]")
+{
+//Arrange
+Game g;
+int pins = 0;
+g.roll(5);
+g.roll(5);
+g.roll(3);
+//Act
+for(int frame = 0; frame < 17; frame++)
+{
+g.roll(pins);
+}
+//Assert
+REQUIRE(16 == g.scoreGame());
 }
